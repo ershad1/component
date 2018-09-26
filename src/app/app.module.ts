6d97b0425edd40430f76300defa4be6718604eb0
ershad <ershad@gmail.com>
2018-09-26 11:55:49 +0600

    Polishing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CounterComponent } from './counter/counter.component';
+import { ProgressBarComponent } from './progress-bar/progress-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterComponent,
+    ProgressBarComponent
   ],
   imports: [
     BrowserModule
